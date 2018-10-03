afd7132f12f35937ca4d6373b99e021d4052c582
Sandeep <skg.sandeepgupta@gmail.com>
2018-10-03 11:19:27 +0530

    added app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// Adding one comment
 var indexRouter = require('./routes/index');
 var trainsRouter = require('./routes/trains');
 
